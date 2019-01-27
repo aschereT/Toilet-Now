f2363afc18a489bec91c00fb2a2c752fc027673b
Vincent Tan <cryptchrysaetos@gmail.com>
2019-01-27 10:09:45 -0800

    add cors exception

--- tn-backend/server.js
+++ tn-backend/server.js
@@ -45,7 +45,7 @@ sql.connect(config).then(pool => {
         //TODO: actually filter
         const result = pool.request().query(`select * from toilets`).then(result => {
             //console.log("Result from DB is " + result.recordsets)
-            return res.set('X-Content-Type-Options', 'nosniff').json({ success: true, data: result.recordsets[0] });
+            return res.set('X-Content-Type-Options', 'nosniff').set('access-control-allow-origin', '*').json({ success: true, data: result.recordsets[0] });
         }).catch(err => {
             console.log("Failed to query database, " + err)
             return res.json({ success: false, error: err });
